d61ed6cd6806373f6aa52d32fbe63a65b15ac7b1
Aleksey Kostov <aleksey_kostov@abv.bg>
2024-02-28 12:09:24 +0200

    Tasks from course JS Front-End

--- JS Syntax Fundamentals - Exercises/vacation.js
+++ JS Syntax Fundamentals - Exercises/vacation.js
@@ -4,28 +4,55 @@ function solve (peopleGroup, typeGroup, dayWeek) {
         case 'Friday':
             if (typeGroup === 'Students') {
                 totalPrice = peopleGroup * 8.45
+                if (peopleGroup >= 30){
+                    totalPrice *= 0.85
+                }
             } else if (typeGroup === 'Business') {
                 totalPrice = peopleGroup * 10.90
+                if (peopleGroup >= 100){
+                    totalPrice -= 10 * 10.90
+                }
             } else if (typeGroup === 'Regular') {
                 totalPrice = peopleGroup * 15
+                if (peopleGroup >= 10 && peopleGroup <=20) {
+                    totalPrice *= 0.95
+                }
             }
             break;
             case 'Saturday':
                 if (typeGroup === 'Students') {
                     totalPrice = peopleGroup * 9.80
+                    if (peopleGroup >= 30){
+                        totalPrice *= 0.85
+                    }
                 } else if (typeGroup === 'Business') {
                     totalPrice = peopleGroup * 15.60
+                    if (peopleGroup >= 100){
+                        totalPrice -= 10 * 15.60
+                    }
                 } else if (typeGroup === 'Regular') {
                     totalPrice = peopleGroup * 20
+                    if (peopleGroup >= 10 && peopleGroup <=20) {
+                        totalPrice *= 0.95
+                    }
                 }
             break;
             case 'Sunday':
                 if (typeGroup === 'Students') {
                     totalPrice = peopleGroup * 10.46
+                    if (peopleGroup >= 30){
+                        totalPrice *= 0.85
+                    }
                 } else if (typeGroup === 'Business') {
                     totalPrice = peopleGroup * 16
+                    if (peopleGroup >= 100){
+                        totalPrice -= 10 * 16
+                    }
                 } else if (typeGroup === 'Regular') {
                     totalPrice = peopleGroup * 22.50
+                    if (peopleGroup >= 10 && peopleGroup <=20) {
+                        totalPrice *= 0.95
+                    }
                 }
             break;
     }
