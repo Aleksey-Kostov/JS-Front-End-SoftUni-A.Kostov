0f694318196e81e496e636d79c22443a952444a7
Aleksey Kostov <aleksey_kostov@abv.bg>
2024-02-29 16:38:48 +0200

    Tasks from course JS Front-End

--- JS Syntax Fundamentals - Exercises/fruit.js
+++ JS Syntax Fundamentals - Exercises/fruit.js
@@ -1,3 +1,7 @@
 function solve (typeFruit, weigth, price) {
-    
+    let weigthKg = 0
+    let money = 0
+    weigthKg = weigth / 1000
+    money = weigthKg * price
+    console.log(`I need $${money.toFixed(2)} to buy ${weigthKg.toFixed(2)} kilograms ${typeFruit}.`);
 }
