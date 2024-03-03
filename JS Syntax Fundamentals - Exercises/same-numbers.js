dfd7f9f7d6e06f27f149e3f35150fb537c4acda7
Aleksey Kostov <aleksey_kostov@abv.bg>
2024-03-03 18:37:17 +0200

    Tasks from course JS Front-End

--- JS Syntax Fundamentals - Exercises/same-numbers.js
+++ JS Syntax Fundamentals - Exercises/same-numbers.js
@@ -1,10 +1,18 @@
 function solve (number){
     let sum = 0
-    let isTrue = false
-    for (i = 0; i <= toString(number).length; i++) {
-        sum += Number(number[i])
+    let isSame = true
+    let stringNum = number.toString()
+    let currentDigit = stringNum[0]
+    for (i = 0; i < stringNum.length; i++) {
+        
+        if (currentDigit !== stringNum[i]) {
+            isSame = false
+        }
 
+        currentDigit = stringNum[i]
+        sum += Number(currentDigit)
     }
+    console.log(isSame);
     console.log(sum);
 }
 
