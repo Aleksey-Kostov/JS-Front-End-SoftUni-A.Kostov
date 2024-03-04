09ee10c85739463a0dd5b703be47ea87e2b72edd
Aleksey Kostov <aleksey_kostov@abv.bg>
2024-03-04 21:21:21 +0200

    Tasks from course JS Front-End

--- JS Arrays and Strings – Lab/count-string-occurrences.js
+++ JS Arrays and Strings – Lab/count-string-occurrences.js
@@ -1,3 +1,13 @@
-function solve () {
-    
+function solve (text, singleWord) {
+    let result = text
+    .split(' ')
+    .filter(element => element === singleWord)
+    .length;
+
+    console.log(result);
+
 }
+
+solve('This is a word and it also is a sentence',
+'is'
+)
