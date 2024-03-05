7abda79545a7a674f132c5968a8d858247a06b63
Aleksey Kostov <aleksey_kostov@abv.bg>
2024-03-05 11:18:55 +0200

    Tasks from course JS Front-End

--- JS-Arrays-and-strings-Exercise/array-rotation.js
+++ JS-Arrays-and-strings-Exercise/array-rotation.js
@@ -1,3 +1,18 @@
-function solve () {
-    
+function solve (numbers, rotations) {
+
+    for (let i = 0; i < rotations; i++) {
+        let firstNumber = numbers[0]
+        let rotatedNumbers = []
+
+        for (let j = 1; j < numbers.length; j++) {
+            rotatedNumbers[j - 1] = numbers[j]
+        }
+
+        rotatedNumbers[rotatedNumbers.length] = firstNumber
+        numbers = rotatedNumbers
+    }
+
+    console.log(numbers.join(' '));
+
 }
+
