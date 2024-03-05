851fe2873c72a5d001aec54476f5046a78839e78
Aleksey Kostov <aleksey_kostov@abv.bg>
2024-03-05 16:54:41 +0200

    Tasks from course JS Front-End

--- JS-Arrays-and-strings-Exercise/reveal-words.js
+++ JS-Arrays-and-strings-Exercise/reveal-words.js
@@ -1,3 +1,23 @@
-function solve () {
+function solve(wordsInput, template) {
+    const words = wordsInput.split(', ')
+    let startIndex = -1
+    let endIndex = -1
+
+    for (let i = 0; i <template.length; i++) {
+        if (template[i] === '*') {
+            if (startIndex < 0) {
+                startIndex = i
+                endIndex = 1
+            } else {
+                endIndex = i
+            }
+        } else {
+            if (startIndex >= 0) {
+                let length = endIndex - startIndex;
+                startIndex = -1
+                endIndex = -1
+            } 
+        }
+    }
     
 }
