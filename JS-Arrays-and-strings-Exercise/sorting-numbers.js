15bbffd4af12f0ce9e7086bb635afa5761646524
Aleksey Kostov <aleksey_kostov@abv.bg>
2024-03-05 16:31:09 +0200

    Tasks from course JS Front-End

--- JS-Arrays-and-strings-Exercise/sorting-numbers.js
+++ JS-Arrays-and-strings-Exercise/sorting-numbers.js
@@ -1,3 +1,19 @@
-function solve () {
+function solve (numbers) {
+    const sortedNumbers = numbers.sort((a, b) => a - b)
+    const result = []
+
+    while (sortedNumbers.length > 0) {
+        let firstNumber = sortedNumbers.shift()
+        let lastNumber = sortedNumbers.pop()
+
+        result.push(firstNumber)
+
+        if (lastNumber) {
+            result.push(lastNumber)
+        }
+
+    }
+
+    return result
     
 }
