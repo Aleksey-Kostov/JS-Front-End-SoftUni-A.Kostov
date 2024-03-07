bcfad4fa4e46fe6fe5cdd1d0dbb7ca934c517d29
Aleksey Kostov <aleksey_kostov@abv.bg>
2024-03-07 21:25:24 +0200

    Tasks from course JS Front-End

--- JS-Arrays-and-strings-Exercise/string-substring.js
+++ JS-Arrays-and-strings-Exercise/string-substring.js
@@ -1,3 +1,14 @@
-function solve () {
-    
+function solve (word, text) {
+   const words = text.toLowerCase().split(' ')
+   const isIncluded = words.includes(word.toLowerCase())
+
+   if (isIncluded) {
+    return word
+   }
+   return `${word} not found!`
 }
+
+const result = solve('javascript',
+'JavaScript is the best programming language'
+)
+console.log(result);
