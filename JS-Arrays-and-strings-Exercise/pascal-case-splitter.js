c62fa3b355de62be8df9a66c2c7379b826d69eaf
Aleksey Kostov <aleksey_kostov@abv.bg>
2024-03-07 21:55:12 +0200

    Tasks from course JS Front-End

--- JS-Arrays-and-strings-Exercise/pascal-case-splitter.js
+++ JS-Arrays-and-strings-Exercise/pascal-case-splitter.js
@@ -1,3 +1,13 @@
-function solve () {
-    
+function solve(input) {
+    const matches = input.matchAll(/[A-Z][a-z]*/g);
+
+   const words =  Array.from(matches).map(match => match[0])
+   console.log(words.join(', '))
+    // for (const match of matches) {
+    //     console.log(match[0])
+    // }
 }
+
+solve('SplitMeIfYouCanHaHaYouCantOrYouCan')
+solve('HoldTheDoor')
+solve('ThisIsSoAnnoyingToDo')
