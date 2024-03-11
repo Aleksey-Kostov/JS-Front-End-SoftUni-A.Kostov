66b22669cc5c90748dfacf95d935112111fee77e
Aleksey Kostov <aleksey_kostov@abv.bg>
2024-03-11 16:56:03 +0200

    Tasks from course JS Front-End

--- JS Functions and Statements - Exercises/password_validator.js
+++ JS Functions and Statements - Exercises/password_validator.js
@@ -1,3 +1,29 @@
-function solve() {
-    
+function solve(password) {
+    const isValidLength = password => password.length >= 6 && password.length <= 10;
+    const isAlphaNumerical = password => /^[a-zA-Z0-9]+$/.test(password)
+    const isStrong = password => password
+        .split('')
+        .filter(character => Number.isInteger(Number(character)))
+        .length >= 2;
+
+    let isValid = true;
+
+    if (!isValidLength(password)) {
+        console.log('Password must be between 6 and 10 characters');
+        isValid = false
+    }
+
+    if (!isAlphaNumerical(password)) {
+        console.log('Password must consist only of letters and digits');
+        isValid = false
+    }
+
+    if (!isStrong(password)) {
+        console.log('Password must have at least 2 digits');
+        isValid = false
+    }
+
+    if (isValid) {
+        console.log('Password is valid');
+    }
 }
