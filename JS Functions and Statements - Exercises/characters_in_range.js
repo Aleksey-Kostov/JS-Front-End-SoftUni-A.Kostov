3f17843a8bf122ebc0c1b86013c63a302a9c2af3
Aleksey Kostov <aleksey_kostov@abv.bg>
2024-03-11 13:52:52 +0200

    Tasks from course JS Front-End

--- JS Functions and Statements - Exercises/characters_in_range.js
+++ JS Functions and Statements - Exercises/characters_in_range.js
@@ -1,3 +1,31 @@
-function solve() {
+
+function solve(firstCharacters, secondCharacters) {
+
+    // const sortedCharacters = getSortedCharacters(firstCharacters, secondCharacters);
+    // const start = sortedCharacters[0]
+    // const end = sortedCharacters[1]
+
+    const [start, end] = getSortedCharacters(firstCharacters, secondCharacters)
+
+    const result = getCharctersBetween(start, end)
+
+    console.log(result.join(' '));
+
+    function getCharctersBetween(start, end) {
+
+        let characters = []
+        for (let i = start.charCodeAt(0) + 1; i < end.charCodeAt(0); i++){
+            characters.push(String.fromCharCode(i))
+        }
     
+        return characters
+    }
+    
+    function getSortedCharacters(a, b) {
+        const characters = [a, b];
+    
+        characters.sort();
+    
+        return characters
+    }
 }
