e0ca14bdc15ded8356608681250a2f59373e41cb
Aleksey Kostov <aleksey_kostov@abv.bg>
2024-03-11 15:03:10 +0200

    Tasks from course JS Front-End

--- JS Functions and Statements - Exercises/odd_and_even_sum.js
+++ JS Functions and Statements - Exercises/odd_and_even_sum.js
@@ -1,3 +1,25 @@
-function solve() {
+function solve(number) {
+
+    const evenSum= calculateDigitsSum(number, x => x % 2 === 0)
+    const oddSum= calculateDigitsSum(number, x => x % 2 !== 0)
+
+   printResult(oddSum, evenSum)
+
+   function calculateDigitsSum(number, filter){
+    const digits = number
+        .toString()
+        .split('')
+        .map(digit => Number(digit))
+        .filter(filter)
+
+    const sum = digits.reduce((acc, digit) => acc + digit, 0)
+
+    return sum
+
+}
+
+function printResult(oddSum, evenSum) {
+    console.log(`Odd sum = ${oddSum}, Even sum = ${evenSum}`);
+}
     
 }
