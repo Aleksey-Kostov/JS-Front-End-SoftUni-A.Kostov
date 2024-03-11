5d302e40d83a8ec3523175eccfe5db8bef9c0347
Aleksey Kostov <aleksey_kostov@abv.bg>
2024-03-11 15:57:03 +0200

    Tasks from course JS Front-End

--- JS Functions and Statements - Exercises/palindrome_integers.js
+++ JS Functions and Statements - Exercises/palindrome_integers.js
@@ -1,3 +1,18 @@
-function solve() {
+
+function solve(numbers) {
+    // numbers.forEach(number => console.log(isPalindrome(number)))
+    numbers.forEach(printPalindromResult)
+
+    function isPalindrome(number) {
+        const forwardNumber = number.toString();
+        const backwardNumber = forwardNumber.split('').reverse().join('');
+    
+        return forwardNumber === backwardNumber
+    
+    }
+    
+    function printPalindromResult(number) {
+        console.log(isPalindrome(number));
+    }
     
 }
