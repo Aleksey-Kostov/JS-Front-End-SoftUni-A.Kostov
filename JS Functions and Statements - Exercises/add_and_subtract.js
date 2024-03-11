9ce72b78410b46955c12a54fbb1c645dc1ad6c72
Aleksey Kostov <aleksey_kostov@abv.bg>
2024-03-11 12:44:26 +0200

    Tasks from course JS Front-End

--- JS Functions and Statements - Exercises/add_and_subtract.js
+++ JS Functions and Statements - Exercises/add_and_subtract.js
@@ -1,3 +1,7 @@
-function solve() {
-    
+function solve(first, second, third) {
+    const sum = (a, b) => a + b;
+    const substract = (a, b) => a - b;
+
+    const result = substract(sum(first, second), third)
+    console.log(result);
 }
