de77228d35d0062084977a15dfadff6ee6946858
Aleksey Kostov <aleksey_kostov@abv.bg>
2024-03-14 23:00:25 +0200

    Tasks from course JS Front-End

--- JS Functions and Statements - Exercises/perfect_number.js
+++ JS Functions and Statements - Exercises/perfect_number.js
@@ -1,3 +1,28 @@
-function solve() {
+function solve(number) {
+    // const divisors = getDivisors(number)
+
+    // const sum = divisors.reduce((a, b) => a + b, 0);
+    const calculateSum = number => number.reduce((a, b) => a + b, 0);
+    const isPerfect = number => calculateSum(getDivisors(number)) === number;
+
+    if (isPerfect(number)) {
+        console.log('We have a perfect number!');
+    } else {
+        console.log('It\'s not so perfect.');
+    }
     
+    function getDivisors(number) {
+        const divisors = []
+    
+        for (let i = number - 1; i >= 0 ; i--){
+            if (number % i === 0) {
+                divisors.push(i)
+            }
+    
+        }
+        return divisors
+    
+    }
 }
+
+solve(6)
