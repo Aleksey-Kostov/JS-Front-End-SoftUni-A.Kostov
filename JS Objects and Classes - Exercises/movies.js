52b4808322f9b0e2b4e6efc82ca08294bd6a0c92
Aleksey Kostov <aleksey_kostov@abv.bg>
2024-03-20 16:57:19 +0200

    Tasks from course JS Front-End

--- JS Objects and Classes - Exercises/movies.js
+++ JS Objects and Classes - Exercises/movies.js
@@ -1,3 +1,39 @@
-function solve() {
+function solve(input) {
+    const movies = []
     
+    for (const row of input) {
+        const addMovieCommand = 'addMovie';
+        const directedByCommand = 'directedBy';
+        const onDateCommand = 'onDate'
+
+        if (row.includes(addMovieCommand)) {
+            const movieName = row.substring(addMovieCommand.length + 1);
+            const movie = {
+                name: movieName,
+            };
+
+            movies.push(movie)
+
+        } else if (row.includes(directedByCommand)) {
+            const [movieName, director] = row.split(` ${directedByCommand} `);
+            const movie = movies.find(movie => movie.name === movieName)
+            
+            if (movie) {
+                movie.director = director
+            }
+
+
+        } else if (row.includes(onDateCommand)) {
+        const [movieName, date] = row.split(` ${onDateCommand} `);
+        const movie = movies.find(movie => movie.name === movieName)
+
+        if (movie) {
+            movie.date = date
+        }
+        }
+    }
+
+    movies
+    .filter(movie => movie.director && movie.date)
+    .forEach(movie => console.log(JSON.stringify(movie)));
 }
