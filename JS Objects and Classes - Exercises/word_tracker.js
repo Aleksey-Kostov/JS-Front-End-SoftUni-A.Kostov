4d387fabd4c6c9a3ac85515848412c945d9b26bb
Aleksey Kostov <aleksey_kostov@abv.bg>
2024-03-22 22:30:19 +0200

    Tasks from course JS Front-End

--- JS Objects and Classes - Exercises/word_tracker.js
+++ JS Objects and Classes - Exercises/word_tracker.js
@@ -1,3 +1,32 @@
-function solve() {
-    
+function solve(input) {
+    const searchWords = input
+    .shift()
+    .split(' ')
+    .reduce((result, word) => {
+        result[word] = 0
+        return result;
+    }, {});
+
+    for (const word of input) {
+        if (searchWords.hasOwnProperty(word)) {
+            searchWords[word] += 1;
+        }
+
+    }
+
+    Object.entries(searchWords)
+    .sort((a, b) => b[1] - a[1])
+    .forEach(([word, occurrences]) => console.log(`${word} - ${occurrences}`))
+
 }
+
+
+solve([
+    'this sentence', 
+    'In', 'this', 'sentence', 'you', 'have', 'to', 'count', 'the', 'occurrences', 'of', 'the', 'words', 'this', 'and', 'sentence', 'because', 'this', 'is', 'your', 'task'
+    ])
+
+solve([
+    'is the', 
+    'first', 'sentence', 'Here', 'is', 'another', 'the', 'And', 'finally', 'the', 'the', 'sentence']
+    )
