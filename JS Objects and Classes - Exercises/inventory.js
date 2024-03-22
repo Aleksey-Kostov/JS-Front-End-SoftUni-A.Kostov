39a892126459c8e5cc0f10807a0c183966eb0af4
Aleksey Kostov <aleksey_kostov@abv.bg>
2024-03-22 21:42:19 +0200

    Tasks from course JS Front-End

--- JS Objects and Classes - Exercises/inventory.js
+++ JS Objects and Classes - Exercises/inventory.js
@@ -1,13 +1,34 @@
-function solve() {
+function solve(input) {
+    const heroes = []
+
+    for (const row of input) {
+        const [name, level, items] = row.split(' / ')
+        const hero = {
+            name, 
+            level: Number(level),
+            items: items,
+        }
+
+        heroes.push(hero)
+    }
+
+    heroes.sort((a, b) => a.level - b.level);
+
+    for (hero of heroes) {
+        console.log(`Hero: ${hero.name}`);
+        console.log(`level => ${hero.level}`);
+        console.log(`items => ${hero.items}`);
+    }
+
 
 }
 
-solve('Isacc / 25 / Apple, GravityGun',
+solve(['Isacc / 25 / Apple, GravityGun',
 'Derek / 12 / BarrelVest, DestructionSword',
-'Hes / 1 / Desolator, Sentinel, Antara'
+'Hes / 1 / Desolator, Sentinel, Antara']
 )
 
-solve('Batman / 2 / Banana, Gun',
+solve(['Batman / 2 / Banana, Gun',
 'Superman / 18 / Sword',
-'Poppy / 28 / Sentinel, Antara'
+'Poppy / 28 / Sentinel, Antara']
 )
