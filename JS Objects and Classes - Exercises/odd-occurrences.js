7fec822897eb010249b50c36edb9d8d2e96bdc05
Aleksey Kostov <aleksey_kostov@abv.bg>
2024-03-22 23:42:17 +0200

    Tasks from course JS Front-End

--- JS Objects and Classes - Exercises/odd-occurrences.js
+++ JS Objects and Classes - Exercises/odd-occurrences.js
@@ -1,3 +1,28 @@
-function solve() {
+function solve(input) {
+    const words = input.split(' ')
+    const result = {}
+
+    for (const word of words) {
+        if (!result.hasOwnProperty(word.toLowerCase())) {
+            result[word.toLowerCase()] = 0;
+        }
+
+
+        result[word.toLowerCase()]++;
+
+    }
+
+    const totralResult = [];
+    for (const word in result) {
+        if (result[word] % 2 !== 0) {
+            totralResult.push(word)
+
+        }
+    }
+    console.log(totralResult.join(' '));
     
 }
+
+solve('Java C# Php PHP Java PhP 3 C# 3 1 5 C#')
+
+solve('Cake IS SWEET is Soft CAKE sweet Food')
