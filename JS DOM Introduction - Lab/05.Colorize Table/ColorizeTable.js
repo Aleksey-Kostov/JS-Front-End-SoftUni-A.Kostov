13afafa4541d90857dfdfb11b7c3180a904d1dc0
Aleksey Kostov <aleksey_kostov@abv.bg>
2024-03-26 12:08:30 +0200

    Tasks from course JS Front-End

--- JS DOM Introduction - Lab/05.Colorize Table/ColorizeTable.js
+++ JS DOM Introduction - Lab/05.Colorize Table/ColorizeTable.js
@@ -1,11 +1,19 @@
+// function colorize() {
+//     const trTableElement = document.querySelectorAll('table tr')
+
+//     for (let i = 0; i < trTableElement.length; i++) {
+//         if (i % 2 !== 0) {
+//             trTableElement[i].style.backgroundColor = 'Teal'
+//         }
+//     }
+
+//     console.log(trTableElement);
+// }
+
+
 function colorize() {
-    const trTableElement = document.querySelectorAll('table tr')
-
-    for (let i = 0; i < trTableElement.length; i++) {
-        if (i % 2 !== 0) {
-            trTableElement[i].style.backgroundColor = 'Teal'
-        }
+        const trTableElement = document.querySelectorAll('table tr:nth-child(2n)')
+        
+        trTableElement.forEach((element) => element.style.backgroundColor = "teal")
+    
     }
-
-    console.log(trTableElement);
-}
