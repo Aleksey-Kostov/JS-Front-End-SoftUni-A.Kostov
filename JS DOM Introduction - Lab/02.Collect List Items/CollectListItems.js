f698505aa85249d44061934891a6820a161df5c7
Aleksey Kostov <aleksey_kostov@abv.bg>
2024-03-26 10:24:26 +0200

    Tasks from course JS Front-End

--- JS DOM Introduction - Lab/02.Collect List Items/CollectListItems.js
+++ JS DOM Introduction - Lab/02.Collect List Items/CollectListItems.js
@@ -1,3 +1,14 @@
 function extractText() {
-    // TODO
+    elementsTextContent = document.querySelectorAll('#items li')
+    textArea = document.getElementById('result')
+    result = ''
+    textArea.value = ''
+
+
+    for (element of elementsTextContent) {
+        result = element.textContent
+        textArea.value += `${result}\n`
+    }
+
+   
 }
