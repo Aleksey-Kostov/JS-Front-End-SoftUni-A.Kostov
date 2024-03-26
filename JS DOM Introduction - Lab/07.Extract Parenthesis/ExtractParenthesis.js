a55f4fffb2d93ce7d238fa154ede28f2d4d3d0a0
Aleksey Kostov <aleksey_kostov@abv.bg>
2024-03-26 13:46:39 +0200

    Tasks from course JS Front-End

--- JS DOM Introduction - Lab/07.Extract Parenthesis/ExtractParenthesis.js
+++ JS DOM Introduction - Lab/07.Extract Parenthesis/ExtractParenthesis.js
@@ -1,3 +1,14 @@
 function extract(content) {
 
+    contentElements = document.getElementById(content)
+
+    const maches = contentElements.textContent.matchAll(/\(([a-zA-Z ]+)\)/g);
+    // const maches = contentElements.textContent.matchAll(/\(([\.]+)\)/g)
+
+    const text = Array.from(maches)
+        .map(match => match[1])
+        .join('; ')
+
+    return text
+
 }
