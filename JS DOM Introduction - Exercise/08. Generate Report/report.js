5d9db7d466788267de245df92d3f892185ebc71b
Aleksey Kostov <aleksey_kostov@abv.bg>
2024-03-26 20:54:21 +0200

    Tasks from course JS Front-End

--- JS DOM Introduction - Exercise/08. Generate Report/report.js
+++ JS DOM Introduction - Exercise/08. Generate Report/report.js
@@ -1,3 +1,69 @@
 function generateReport() {
-    //TODO
+    
+    const thInputElements = document.querySelectorAll('input');
+    const trBodyElement = document.querySelectorAll('table tbody tr');
+    const textAreaElement = document.getElementById('output');
+ 
+    let checkedMap = [];
+    let reportInfo = [];
+ 
+    /* let checkedMap1 = Array.from(thInputElements).map(({}, index) => ({[thInputElements[index].name]: thInputElements[index].checked})); */
+    /* for (let i=0; i<thInputElements.length; i++) {
+        checkedMap.push({
+            [thInputElements[i].name]:thInputElements[i].checked
+        })
+    } */
+ 
+    for (let i=0; i<thInputElements.length; i++) {
+        let inputName = thInputElements[i].name;
+        let isChecked = thInputElements[i].checked;
+        checkedMap.push([inputName, isChecked])
+    }
+ 
+    for (let i=0; i<trBodyElement.length; i++) {
+        let dataFromRow = trBodyElement[i].querySelectorAll('td');
+        reportInfo.push({});
+ 
+        for (let j=0; j<checkedMap.length; j++) {
+            if (checkedMap[j][1] === true) {
+                reportInfo[i][checkedMap[j][0]] = dataFromRow[j].textContent
+            }
+        }
+    }
+ 
+    textAreaElement.textContent = JSON.stringify(reportInfo, null, 2)
+    /* console.dir(checkedMap);
+    console.dir(reportInfo); */
 }
+ 
+/* function generateReport() {
+    //TODO
+    const thElement = document.querySelectorAll('table thead tr th');
+    const trBodyElement = document.querySelectorAll('table tbody tr');
+    const textAreaElement = document.getElementById('output')
+ 
+    let checkedMap = [];
+    let reportInfo = [];
+ 
+    for (let thE of thElement) {
+        let checkBox = thE.querySelector('input[type=checkbox]');
+        let isChecked = checkBox.checked;
+        checkedMap.push({[thE.textContent.toLowerCase().trim()]:isChecked});
+    }
+ 
+    for (let i=0; i<trBodyElement.length; i++) {
+        let dataFromRow = trBodyElement[i].querySelectorAll('td');
+        reportInfo.push({});
+ 
+        for (let j=0; j<checkedMap.length; j++) {
+            if (Object.values(checkedMap[j])[0] === true) {
+                reportInfo[i][Object.keys(checkedMap[j])[0]] = dataFromRow[j].textContent
+            }
+        }
+    }
+ 
+    console.dir(reportInfo)
+    console.dir(JSON.stringify(reportInfo))
+    textAreaElement.textContent = JSON.stringify(reportInfo, null, 2)
+    textAreaElement.value = JSON.stringify(reportInfo, null, 2)
+} */
