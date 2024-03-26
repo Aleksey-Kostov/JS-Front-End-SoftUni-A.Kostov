04da5d3a6060de54f06e947728e1d6aba8b7a4da
Aleksey Kostov <aleksey_kostov@abv.bg>
2024-03-26 12:48:14 +0200

    Tasks from course JS Front-End

--- JS DOM Introduction - Lab/06.Sum Table/SumTable.js
+++ JS DOM Introduction - Lab/06.Sum Table/SumTable.js
@@ -1,3 +1,12 @@
 function sumTable() {
+    const productElements = document.querySelectorAll('tr td:last-child:not(#sum)')
+    const tdSumElement = document.getElementById('sum')
+
+    
+    const sum = Array
+        .from(productElements)
+        .reduce((sum, element) => sum + Number(element.textContent), 0)
+
+    tdSumElement.textContent = sum
 
 }
