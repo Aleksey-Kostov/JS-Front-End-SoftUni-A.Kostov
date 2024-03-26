26e7681f30e167ca5c94aded16da2bed875074ee
Aleksey Kostov <aleksey_kostov@abv.bg>
2024-03-26 16:56:01 +0200

    Tasks from course JS Front-End

--- JS DOM Introduction - Exercise/07. Hell's Kitchen/app.js
+++ JS DOM Introduction - Exercise/07. Hell's Kitchen/app.js
@@ -1,8 +1,33 @@
 function solve() {
    document.querySelector('#btnSend').addEventListener('click', onClick);
+   textAreaElements = document.querySelector('textarea')
+   texts = []
+   texts = textAreaElements.value.match(/"(.*?)"/g)
+   console.log(texts);
+
+   
+
+   for (text of texts) {
+      // const currentText = JSON.parse(texts)
+      // console.log(currentText);
+      // const currentObject = {}
+      // const key = currentText[0]
+      // const value = currentText[1]
+      console.log(text);
+
+
+      // currentObject[key] = value
+      // console.log(text);
+   }
+
+
+
+   
+
+
 
    function onClick () {
-      //   TODO:
+      
       
    }
 }
