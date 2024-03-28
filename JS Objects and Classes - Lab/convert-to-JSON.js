0d6e9cf76513976ecdff07b3dd2b3f95a57de896
Aleksey Kostov <aleksey_kostov@abv.bg>
2024-03-28 16:44:06 +0200

    Tasks from course JS Front-End

---
+++ JS Objects and Classes - Lab/convert-to-JSON.js
@@ -0,0 +1,9 @@
+function solve(firstName, lastName, hairColor) {
+    newObject = {
+        name: firstName,
+        lastName,
+        hairColor
+    }
+
+    console.log(JSON.stringify(newObject));
+}
