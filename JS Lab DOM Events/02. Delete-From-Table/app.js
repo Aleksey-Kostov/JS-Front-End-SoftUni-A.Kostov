b92a1d9726e7af3d727077d48c6e1fa777108a1a
Aleksey Kostov <aleksey_kostov@abv.bg>
2024-03-28 22:27:09 +0200

    Tasks from course JS Front-End

--- JS Lab DOM Events/02. Delete-From-Table/app.js
+++ JS Lab DOM Events/02. Delete-From-Table/app.js
@@ -1,3 +1,18 @@
 function deleteByEmail() {
-    console.log('TODO:...');
+    const inputElement = document.querySelector('input')
+    const buttonDelete = document.querySelector('button')
+    const mail = document.querySelectorAll('table tbody tr')
+    const resultElement = document.getElementById('result');
+
+    const resultTrElement = Array
+        .from(mail).find(trElement => trElement.children[1].textContent.includes(inputElement.value))
+    if (resultTrElement) {
+        // Delete table row
+        resultTrElement.remove();
+        
+        // Show deleted text
+        resultElement.textContent = 'Deleted.';
+    } else {
+        resultElement.textContent = 'Not found.';
+    }
 }
