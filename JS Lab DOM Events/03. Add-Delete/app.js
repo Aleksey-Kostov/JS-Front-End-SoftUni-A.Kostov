8f7a72585f4c99f71b4176bd8899bb3bf92c4ee8
Aleksey Kostov <aleksey_kostov@abv.bg>
2024-03-29 15:34:56 +0200

    Tasks from course JS Front-End

--- JS Lab DOM Events/03. Add-Delete/app.js
+++ JS Lab DOM Events/03. Add-Delete/app.js
@@ -1,3 +1,24 @@
 function addItem() {
-    //TODO...
+    const itemsElemets = document.getElementById('items')
+    const textContentInputElemnts = document.getElementById('newItemText')
+
+    const newItemElement = document.createElement('li')
+    const newItemElementA = document.createElement('a')
+    newItemElement.textContent = textContentInputElemnts.value
+    newItemElementA.textContent = '[Delete]'
+    newItemElementA.href = '#'
+
+    newItemElementA.addEventListener('click', () => {
+        newItemElement.remove();
+    });
+
+    newItemElement.appendChild(newItemElementA)
+    itemsElemets.appendChild(newItemElement)
+
+    
+    
+
+    textContentInputElemnts.value = ''
+    
+
 }
