29afe714c5481afd6b44679145376c4cbd69eb3d
Aleksey Kostov <aleksey_kostov@abv.bg>
2024-03-29 15:58:57 +0200

    Tasks from course JS Front-End

--- JS Lab DOM Events/05. Highlight-Active/app.js
+++ JS Lab DOM Events/05. Highlight-Active/app.js
@@ -1,3 +1,11 @@
 function focused() {
-    console.log('TODO:...');
+    const inputFields = document.querySelectorAll('input[type=text]')
+
+    Array.from(inputFields).forEach(inputElement => inputElement.addEventListener('focus', (event) => {
+        event.target.parentElement.classList.add('focused');
+    }));
+
+    Array.from(inputFields).forEach(inputElement => inputElement.addEventListener('blur', (event) => {
+        event.target.parentElement.classList.remove('focused');
+    }));
 }
