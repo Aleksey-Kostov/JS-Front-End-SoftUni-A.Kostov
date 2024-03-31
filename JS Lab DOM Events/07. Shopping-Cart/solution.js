3e9bd6fc3f894644c5af362add6ea4965211c625
Aleksey Kostov <aleksey_kostov@abv.bg>
2024-03-31 20:56:06 +0300

    Tasks from course JS Front-End

--- JS Lab DOM Events/07. Shopping-Cart/solution.js
+++ JS Lab DOM Events/07. Shopping-Cart/solution.js
@@ -1,3 +1,31 @@
 function solve() {
-   //TODO...
+   const addButton = document.querySelectorAll('button.add-product')
+   const textAreaField = document.querySelector('textarea')
+   const checkOut = document.querySelector('button.checkout')
+
+   let totalPrice = 0;
+   let uniqueProducts = {};
+
+   for (const buttonElement of addButton) {
+      const productElement = buttonElement.parentElement.parentElement;
+
+      buttonElement.addEventListener('click', () => {
+         const title = productElement.querySelector('.product-title').textContent;
+         const price = Number(productElement.querySelector('.product-line-price').textContent);
+
+         totalPrice += price;
+         uniqueProducts[title] = true;
+
+         textAreaField.textContent += `Added ${title} for ${price.toFixed(2)} to the cart.\n`;
+      });
+   }
+
+   checkOut.addEventListener('click', (event) => {
+      Array.from(addButton).forEach(buttonElement => buttonElement.setAttribute('disabled', 'disabled'))
+      event.target.setAttribute('disabled', 'disabled');
+      const list = Object.keys(uniqueProducts).join(', ');
+
+      textAreaField.textContent += `You bought ${list} for ${totalPrice.toFixed(2)}.\n`;
+   });
+
 }
