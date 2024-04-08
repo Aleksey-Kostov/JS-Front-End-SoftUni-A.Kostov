7db9ed3809f841483e1053e4cba43a5aa3d17dc1
Aleksey Kostov <aleksey_kostov@abv.bg>
2024-04-08 12:05:26 +0300

    Tasks from course JS Front-End

---
+++ Exam Preparation/01. Cafeteria.js
@@ -0,0 +1,49 @@
+function solve(input) {
+    const num = Number(input.shift())
+    let persons = {}
+
+    for (let i = 0; i < 3; i++) {
+        const [baristaName, shift, ...coffee] = input.shift()
+        persons = {
+            Barista: baristaName,
+            shift,
+            drinks: coffee,
+        }
+
+    }
+
+    console.log(persons);
+
+
+}
+
+solve([
+    '3',
+      'Alice day Espresso,Cappuccino',
+      'Bob night Latte,Mocha',
+      'Carol day Americano,Mocha',
+      'Prepare / Alice / day / Espresso',
+      'Change Shift / Bob / night',
+      'Learn / Carol / Latte',
+      'Learn / Bob / Latte',
+      'Prepare / Bob / night / Latte',
+      'Closed']
+    )
+
+
+solve(['4',
+'Alice day Espresso,Cappuccino',
+'Bob night Latte,Mocha',
+'Carol day Americano,Mocha',
+'David night Espresso',
+'Prepare / Alice / day / Espresso',
+'Change Shift / Bob / day',
+'Learn / Carol / Latte',
+'Prepare / Bob / night / Latte',
+'Learn / David / Cappuccino',
+'Prepare / Carol / day / Cappuccino',
+'Change Shift / Alice / night',
+ 'Learn / Bob / Mocha',
+'Prepare / David / night / Espresso',
+'Closed'];
+)
