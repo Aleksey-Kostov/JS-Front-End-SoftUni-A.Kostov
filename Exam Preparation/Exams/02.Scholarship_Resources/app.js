0cf09596cb4f0d4fa9beea344a7e4eff3585e0df
Aleksey Kostov <aleksey_kostov@abv.bg>
2024-04-09 19:36:41 +0300

    Tasks from course JS Front-End

--- Exam Preparation/Exams/02.Scholarship_Resources/app.js
+++ Exam Preparation/Exams/02.Scholarship_Resources/app.js
@@ -5,9 +5,82 @@ function solve() {
   const inputTextUniversityElement = document.getElementById('university')
   const inputTextScoreElement = document.getElementById('score')
   const buttonNextElement = document.getElementById('next-btn')
+  const previewListElement = document.getElementById('preview-list')
+  const candidatesListElement = document.getElementById('candidates-list')
 
   buttonNextElement.addEventListener('click', () => {
+    const articleElement = document.createElement('article')
+    const h4Element = document.createElement('h4')
+    const pUniversityElement = document.createElement('p')
+    const pScoreElement = document.createElement('p')
+
+    const textContentH4Element = inputTextStudentElement.value
+    const textContentPUniversityElement = inputTextUniversityElement.value
+    const textContentPScoreElement = inputTextScoreElement.value
+
+    h4Element.textContent = `${textContentH4Element}`
+    pUniversityElement.textContent = `University: ${textContentPUniversityElement}`
+    pScoreElement.textContent = `Score: ${textContentPScoreElement}`
+
+    articleElement.appendChild(h4Element)
+    articleElement.appendChild(pUniversityElement)
+    articleElement.appendChild(pScoreElement)
+
+    const btnEdit = document.createElement('button')
+    const btnApply = document.createElement('button')
+
+    btnEdit.classList.add('action-btn', 'edit')
+    btnApply.classList.add('action-btn', 'apply')
+
+    btnEdit.textContent = 'edit'
+    btnApply.textContent = 'apply'
+
+    const liElement = document.createElement('li')
+
+    liElement.classList.add('application')
+
+    liElement.appendChild(articleElement)
+    liElement.appendChild(btnEdit)
+    liElement.appendChild(btnApply)
     
+    if (inputTextStudentElement.value === '' ||
+    inputTextUniversityElement.value === '' ||
+    inputTextScoreElement.value === '') {
+      return
+    }
+
+    previewListElement.appendChild(liElement)
+
+    buttonNextElement.setAttribute('disabled', 'disabled')
+    inputTextStudentElement.value = ''
+    inputTextUniversityElement.value = ''
+    inputTextScoreElement.value = ''
+
+    btnEdit.addEventListener ('click', () => {
+      inputTextStudentElement.value = textContentH4Element
+      inputTextUniversityElement.value = textContentPUniversityElement
+      inputTextScoreElement.value = textContentPScoreElement
+      previewListElement.innerHTML = ''
+      buttonNextElement.removeAttribute('disabled')
+      
+
+
+
+    })
+
+    btnApply.addEventListener ('click', () => {
+
+      previewListElement.innerHTML = ''
+
+      btnEdit.remove()
+      btnApply.remove()
+
+      candidatesListElement.appendChild(liElement)
+      buttonNextElement.removeAttribute('disabled')
+      
+
+    })
+
 
   })
 
