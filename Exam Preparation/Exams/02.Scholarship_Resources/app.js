997dbb3eedf750a41c7984f158b7842de47ab486
Aleksey Kostov <aleksey_kostov@abv.bg>
2024-04-09 16:30:32 +0300

    Tasks from course JS Front-End

--- Exam Preparation/Exams/02.Scholarship_Resources/app.js
+++ Exam Preparation/Exams/02.Scholarship_Resources/app.js
@@ -1,7 +1,14 @@
 window.addEventListener("load", solve);
 
 function solve() {
+  const inputTextStudentElement = document.getElementById('student')
+  const inputTextUniversityElement = document.getElementById('university')
+  const inputTextScoreElement = document.getElementById('score')
+  const buttonNextElement = document.getElementById('next-btn')
+
+  buttonNextElement.addEventListener('click', () => {
     
-  
-  }
-  
+
+  })
+
+}
