59376ed54687aa1d3a1c9020eb30222d821316a2
Aleksey Kostov <aleksey_kostov@abv.bg>
2024-04-11 16:49:16 +0300

    Tasks from course JS Front-End

--- Exam Preparation/Exams/01. Sprint Review.js
+++ Exam Preparation/Exams/01. Sprint Review.js
@@ -1,18 +1,26 @@
 function solve(input) {
     const n = Number(input.shift())
-    persons = []
-
+    const persons = {}
+   
     for (let i = 0; i < n; i++) {
         const [name, personId, title, status, points] = input.shift().split(':')
-        name = {
-            personId,
-            title,
-            status,
-            points
-        }
-        persons.push(name)
 
+        if (!persons.hasOwnProperty(name)) {
+            persons[name] = []
+        }
+
+        persons[name].push({personId, title, status, points: Number(points)})
     }
+        
+    //     const currentPerson = {}
+    //     currentPerson[name] = {
+    //         personId,
+    //         title,
+    //         status,
+    //         points
+    //     }
+    //     persons.push(currentPerson)
+    // }
 
     // let command = input.shift().split(' ')
 
@@ -21,36 +29,62 @@ function solve(input) {
 
         if (command === 'Add New') {
             const [assignee, taskId, title, status, estimatedPoints] = args
-            if (assignee in persons) {
-                persons[assignee] = [taskId, title, status, estimatedPoints]
+
+            if (persons.hasOwnProperty(assignee)) {
+                persons[assignee].push({ personId: taskId, title, status, points: Number(estimatedPoints) })
             } else {
                 console.log(`Assignee ${assignee} does not exist on the board!`);
             }
-
         } else if (command === 'Change Status') {
             const [assignee, taskId, newStatus] = args
-            if (persons[assignee] in persons) {
-                if (persons[assignee][0] !== taskId) {
+            if (persons.hasOwnProperty(assignee)) {
+                if (persons[assignee].personId !== taskId) {
                     console.log(`Task with ID ${taskId} does not exist for ${assignee}!`);
                 } else {
-                    persons[assignee][2] = newStatus
+                    persons[assignee].status = newStatus
                 }
             } else {
                 console.log(`Assignee ${assignee} does not exist on the board!`);
             }
-
-
         } else if (command === 'Remove Task') {
             const [assignee, index] = args
-            if (assignee in persons && index < persons[assignee].length){}
+            if(!persons.hasOwnProperty(assignee)) {
+                console.log(`Assignee ${assignee} does not exist on the board!`);
+            } else if((persons[assignee].length - 1) !== Number(index)) {
 
+                console.log(persons[assignee].length - 1);
+                console.log('Index is out of range!');
+            } else {
+                persons[assignee].splice(Number(index), 1)
+
+            }
         }
     }
 
+    for (let person in persons) {
+        for (let task in persons[person]) {
+            let toDoTasksTotalPoints = 0
+            let inProgressTasksTotalPoints = 0
+            let codeReviewTasksTotalPoints = 0
+            let doneTasksTotalPoints = 0
 
+            if (persons[person][task].status === 'ToDo'){
+                toDoTasksTotalPoints += persons[person][task].points
+                console.log(`ToDo: ${toDoTasksTotalPoints}pts`);
+            }else if (persons[person][task].status === 'In Progress') {
+                inProgressTasksTotalPoints += persons[person][task].points
+                console.log(`In Progress: ${inProgressTasksTotalPoints}pts`)
+            }else if (persons[person][task].status === 'Code Review') {
+                codeReviewTasksTotalPoints += persons[person][task].points
+                console.log(`Code Review: ${codeReviewTasksTotalPoints}pts`);
+            }else if (persons[person][task].status === 'Done Points') {
+                doneTasksTotalPoints += persons[person][task].points
+                console.log(`Done Points: ${doneTasksTotalPoints}pts`);
+            }
 
-
-
+        }
+       
+    } 
 }
 
 
