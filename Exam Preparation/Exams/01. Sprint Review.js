f882ab5cd5575d331b2b637c983336496529089e
Aleksey Kostov <aleksey_kostov@abv.bg>
2024-04-11 20:35:16 +0300

    Tasks from course JS Front-End

--- Exam Preparation/Exams/01. Sprint Review.js
+++ Exam Preparation/Exams/01. Sprint Review.js
@@ -11,18 +11,7 @@ function solve(input) {
 
         persons[name].push({personId, title, status, points: Number(points)})
     }
-        
-    //     const currentPerson = {}
-    //     currentPerson[name] = {
-    //         personId,
-    //         title,
-    //         status,
-    //         points
-    //     }
-    //     persons.push(currentPerson)
-    // }
-
-    // let command = input.shift().split(' ')
+ 
 
     for (let i = 0; i < input.length; i++) {
         const [command, ...args] = input[i].split(':')
@@ -35,70 +24,106 @@ function solve(input) {
             } else {
                 console.log(`Assignee ${assignee} does not exist on the board!`);
             }
+
         } else if (command === 'Change Status') {
             const [assignee, taskId, newStatus] = args
+
             if (persons.hasOwnProperty(assignee)) {
-                if (persons[assignee].personId !== taskId) {
+
+                const findID = persons[assignee].find((t) => t.personId === taskId)
+
+                if (!findID) {
                     console.log(`Task with ID ${taskId} does not exist for ${assignee}!`);
                 } else {
-                    persons[assignee].status = newStatus
-                }
+                    findID.status = newStatus
+                }       
             } else {
                 console.log(`Assignee ${assignee} does not exist on the board!`);
             }
         } else if (command === 'Remove Task') {
             const [assignee, index] = args
+            
             if(!persons.hasOwnProperty(assignee)) {
                 console.log(`Assignee ${assignee} does not exist on the board!`);
-            } else if((persons[assignee].length - 1) !== Number(index)) {
+            } 
 
-                console.log(persons[assignee].length - 1);
-                console.log('Index is out of range!');
-            } else {
-                persons[assignee].splice(Number(index), 1)
-
+            else if (persons.hasOwnProperty(assignee)) {
+                if ((persons[assignee].length) <= Number(index) || Number(index) < 0) {
+                    console.log('Index is out of range!')
+                } else {
+                    persons[assignee].splice(Number(index), 1)
+                }
             }
         }
     }
 
+    let toDoTasksTotalPoints = 0
+    let inProgressTasksTotalPoints = 0
+    let codeReviewTasksTotalPoints = 0
+    let doneTasksTotalPoints = 0
+
     for (let person in persons) {
         for (let task in persons[person]) {
-            let toDoTasksTotalPoints = 0
-            let inProgressTasksTotalPoints = 0
-            let codeReviewTasksTotalPoints = 0
-            let doneTasksTotalPoints = 0
 
             if (persons[person][task].status === 'ToDo'){
                 toDoTasksTotalPoints += persons[person][task].points
-                console.log(`ToDo: ${toDoTasksTotalPoints}pts`);
+                
             }else if (persons[person][task].status === 'In Progress') {
                 inProgressTasksTotalPoints += persons[person][task].points
-                console.log(`In Progress: ${inProgressTasksTotalPoints}pts`)
+                
             }else if (persons[person][task].status === 'Code Review') {
                 codeReviewTasksTotalPoints += persons[person][task].points
-                console.log(`Code Review: ${codeReviewTasksTotalPoints}pts`);
-            }else if (persons[person][task].status === 'Done Points') {
+                
+            }else if (persons[person][task].status === 'Done') {
                 doneTasksTotalPoints += persons[person][task].points
-                console.log(`Done Points: ${doneTasksTotalPoints}pts`);
+                
             }
 
         }
        
-    } 
+    }
+
+    console.log(`ToDo: ${toDoTasksTotalPoints}pts`);
+    console.log(`In Progress: ${inProgressTasksTotalPoints}pts`);
+    console.log(`Code Review: ${codeReviewTasksTotalPoints}pts`);
+    console.log(`Done Points: ${doneTasksTotalPoints}pts`);
+
+    const totalSumOtherTasks = toDoTasksTotalPoints + inProgressTasksTotalPoints + codeReviewTasksTotalPoints
+
+    if (doneTasksTotalPoints >= totalSumOtherTasks) {
+        console.log('Sprint was successful!');
+    } else {
+        console.log('Sprint was unsuccessful...');
+    }
+
 }
 
 
 
-solve(    [
-    '5',
-    'Kiril:BOP-1209:Fix Minor Bug:ToDo:3',
-    'Mariya:BOP-1210:Fix Major Bug:In Progress:3',
-    'Peter:BOP-1211:POC:Code Review:5',
-    'Georgi:BOP-1212:Investigation Task:Done:2',
-    'Mariya:BOP-1213:New Account Page:In Progress:13',
-    'Add New:Kiril:BOP-1217:Add Info Page:In Progress:5',
-    'Change Status:Peter:BOP-1290:ToDo',
-    'Remove Task:Mariya:1',
-    'Remove Task:Joro:1',
+// solve(    [
+//     '5',
+//     'Kiril:BOP-1209:Fix Minor Bug:ToDo:3',
+//     'Mariya:BOP-1210:Fix Major Bug:In Progress:3',
+//     'Peter:BOP-1211:POC:Code Review:5',
+//     'Georgi:BOP-1212:Investigation Task:Done:2',
+//     'Mariya:BOP-1213:New Account Page:In Progress:13',
+//     'Add New:Kiril:BOP-1217:Add Info Page:In Progress:5',
+//     'Change Status:Peter:BOP-1290:ToDo',
+//     'Remove Task:Mariya:1',
+//     'Remove Task:Joro:1',
+// ]
+// )
+
+solve(  [
+    '4',
+    'Kiril:BOP-1213:Fix Typo:Done:1',
+    'Peter:BOP-1214:New Products Page:In Progress:2',
+    'Mariya:BOP-1215:Setup Routing:ToDo:8',
+    'Georgi:BOP-1216:Add Business Card:Code Review:3',
+    'Add New:Sam:BOP-1237:Testing Home Page:Done:3',
+    'Change Status:Georgi:BOP-1216:Done',
+    'Change Status:Will:BOP-1212:In Progress',
+    'Remove Task:Georgi:3',
+    'Change Status:Mariya:BOP-1215:Done',
 ]
 )
